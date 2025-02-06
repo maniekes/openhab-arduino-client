597d5605a22582ba4ac7f90d20420bbe53b334b4
Maniekes <maniekes@gmail.com>
2025-02-06 17:33:07 +0100

    better example code

--- examples/oh_test/oh_test.ino
+++ examples/oh_test/oh_test.ino
@@ -1,24 +1,42 @@
-#include <OHClient.h>
+#include <OpenHABClient.h>
 
-const char* ssid = "YourWiFiSSID";
-const char* password = "YourWiFiPassword";
-OpenHABClient openhab("192.168.1.100");  // Replace with your OpenHAB server IP
+
+#define WIFI_SSID "WIFI_SSID"
+#define WIFI_PASS "WIFI_PASS"
+#define OH_TOKEN "your-token-here"
+#define OH_HOST "https://openhab.server:9090"
+
+OpenHABClient *ohclient;
+
+void connectToWiFi() {
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  Serial.print("Connecting to WiFi...");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println("\nConnected to WiFi!");
+}
+
+void initOH() {
+  ohclient = new OpenHABClient(OH_HOST);
+  ohclient->setBearerToken(OH_TOKEN);
+}
+
 
 void setup() {
     Serial.begin(115200);
-
-    Serial.println("Getting OpenHAB item state...");
-    String state = openhab.getItemState("LivingRoom_Light");
-    Serial.println("LivingRoom_Light state: " + state);
-
-    Serial.println("Sending command to turn ON light...");
-    if (openhab.sendCommand("LivingRoom_Light", "ON")) {
-        Serial.println("Light turned ON!");
-    } else {
-        Serial.println("Failed to send command.");
-    }
+    connectToWiFi();
+    initOH();
 }
 
 void loop() {
-    // Nothing needed here for this example
+  String item1 = ohclient->getItemState("item_1");
+  bool item2 = ohclient->getItemStateBoolean("item_2");
+  long item3 = ohclient->getItemStateLong("item_3");
+  Serial.printf("item1: %s, item2: %d, item3: %.2f\n", item1, item2, item3);
+  ohclient->updateItemState("item_3", ++item3);
+  ohclient->sendCommand("item_2", "TOGGLE");
+  delay(10000);
 }
